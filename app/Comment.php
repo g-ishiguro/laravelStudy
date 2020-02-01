8a046d9bca61405f57567f65e3bf0527587faab8
g.ishiguro <genki.i0918@gmail.com>
2020-02-01 23:29:28 +0900

    #29  完了

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['(body'];
+    protected $fillable = ['body'];
 
     // $comment->post
     public function post() {
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,5 +17,19 @@
     </li>
     @empty
     <li>No comment yet</li>
-    @endforelse </ul>
+    @endforelse 
+</ul>
+<form action="{{ action('CommentsController@store', $post) }}" method="post">
+    {{ csrf_field() }}
+    <p>
+        <input type="text" name='body' placeholder='enter comment' value="{{ old('title') }}">
+        @if($errors->has('body'))
+        <span class='error'>{{ $errors->first('body') }}</span>
+        @endif
+    </p>
+    <p>
+        <input type="submit" value='add Comment'>
+    </p>
+    <p></p>
+</form>
 @endsection
